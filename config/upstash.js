c1e23421f85cb91f0f09f7e5864495106fe8805a
GaduhH <79295302+GaduhH@users.noreply.github.com>
2025-07-14 10:09:06 +0700

    Update upstash.js

--- config/upstash.js
+++ config/upstash.js
@@ -1,11 +1,10 @@
-import { Redis } from "@upstash/redis";
-import { Ratelimit } from "@upstash/ratelimit";
-
-import "dotenv/config";
+const { Redis } = require("@upstash/redis");
+const { Ratelimit } = require("@upstash/ratelimit");
+require("dotenv").config();
 
 const ratelimiter = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
 
-export default ratelimiter;
+module.exports = ratelimiter;
